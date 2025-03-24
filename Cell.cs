e6d16238fa0f936bebe707ef4c2ce99ad665b167
Kimberly-git53 <kimalvarez2014@gmail.com>
2025-03-23 21:05:35 -0500

    Add files via upload

---
+++ Cell.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeperClasses
+{
+    public class Cell
+    {
+        // Properties
+        public int Row { get; set; } = -1;
+        public int Col { get; set; } = -1;
+        public bool IsVisited { get; set; } = false;
+        public bool IsBomb { get; set; } = false;
+        public bool IsFlagged { get; set; } = false;
+        public int NumberOfBombNeighbors { get; set; } = 0;
+        public bool HasSpecialReward { get; set; } = false;
+
+        // Constructor
+        public Cell(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+        // Method to determine if special reward is available
+        public void Undo()
+        {
+            // We will implement this method later
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using MineSweeperClasses;
+
+namespace MineSweeperConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to Minesweeper");
+            // size 10 and difficulty 0.1
+            Board board = new Board(10, 0.1f);
+            Console.WriteLine("Here is the answer key for the first board.");
+            PrintAnswers(board);
+
+            // Size 15 and difficulty 0.15
+            board = new Board(15, 0.15f);
+            Console.WriteLine("Here is the answer key for the second board.");
+            PrintAnswers(board);
+
+
+        }
+        // Method to print the board answers
+        static void PrintAnswers(Board board)
+        {
+            // Print column numbers
+            Console.WriteLine(" ");
+            for (int col = 0; col < board.Size; col++)
+            {
+                Console.Write(" " + col);
+            }
+            Console.WriteLine();
+
+            // Print divider line
+            Console.WriteLine(new string('-', board.Size * 2 + 3));
+
+            // Print the rows and cells
+            for (int row = 0; row < board.Size; row++)
+            {
+                // Print row number
+                Console.Write($"{row}| ");
+                // Print the cells
+                for (int col = 0; col < board.Size; col++)
+                {
+                    // Print the bomb if it is a bomb
+                    if (board.Cells[row, col].IsBomb)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("B ");
+                    }
+                    // Print the number 1-8 if it is not a bomb
+                    else if (board.Cells[row, col].NumberOfBombNeighbors == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write($"{board.Cells[row, col].NumberOfBombNeighbors} ");
+                    }
+                    else if (board.Cells[row, col].NumberOfBombNeighbors == 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write($"{board.Cells[row, col].NumberOfBombNeighbors} ");
+                    }
+                    else if (board.Cells[row, col].NumberOfBombNeighbors == 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.Write($"{board.Cells[row, col].NumberOfBombNeighbors} ");
+                    }
+                    else if (board.Cells[row, col].NumberOfBombNeighbors == 4)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkBlue;
+                        Console.Write($"{board.Cells[row, col].NumberOfBombNeighbors} ");
+                    }
+                    else if (board.Cells[row, col].NumberOfBombNeighbors == 5)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.Write($"{board.Cells[row, col].NumberOfBombNeighbors} ");
+                    }
+                    else if (board.Cells[row, col].NumberOfBombNeighbors == 6)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Console.Write($"{board.Cells[row, col].NumberOfBombNeighbors} ");
+                    }
+                    else if (board.Cells[row, col].NumberOfBombNeighbors == 7)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                        Console.Write($"{board.Cells[row, col].NumberOfBombNeighbors} ");
+                    }
+                    else if (board.Cells[row, col].NumberOfBombNeighbors == 8)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write($"{board.Cells[row, col].NumberOfBombNeighbors} ");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(". ");
+                    }
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(new string('-', board.Size * 2 + 3));
+        }
+    }
+}
